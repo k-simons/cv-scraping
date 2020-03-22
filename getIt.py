fee5cf89d394f48b0ab49c0cf74012247dec8a53
Kevin Simons <ksimons@palantir.com>
2020-03-22 11:09:26 -0700

    Better test name

--- getIt.py
+++ getIt.py
@@ -1,18 +1,31 @@
 from bs4 import BeautifulSoup
+import json
+import time
+from datetime import datetime
 import urllib.request
 
 def main():
-    page = urllib.request.urlopen("https://www.worldometers.info/coronavirus/country/us/#nav-yesterday")
+    page = urllib.request.urlopen("https://www.worldometers.info/coronavirus/country/us")
     soup = BeautifulSoup(page, 'html.parser')
-    a = soup.find("table", {"id": "usa_table_countries_today"})
-    c = a.find('tbody')
-    trs = c.find_all('tr')
-    singleTr = trs[0]
-    singleThing(singleTr)
-# print(soup.prettify())
+    usaTable = soup.find("table", {"id": "usa_table_countries_today"})
+    tableBody = usaTable.find('tbody')
+    tableRows = tableBody.find_all('tr')
+    while len(tableRows) < 20:
+        print(len(tableRows))
+        time.sleep(1)
+        tableBody = usaTable.find('tbody')
+        tableRows = tableBody.find_all('tr')
+    rowResults = []
+    for tableRow in tableRows:
+        rowResult = singleRowToRowResult(tableRow)
+        rowResults.append(rowResult)
+    singleStateScrape = SingleStateScrape(rowResults)
+    f = open("demofile3.txt", "w")
+    f.write(singleStateScrape.serialize())
+    f.close()
 
 
-def singleThing(singleTr):
+def singleRowToRowResult(singleTr):
     tds = singleTr.find_all('td')
     state = tds[0]
     totalCases = tds[1]
@@ -22,19 +35,13 @@ def singleThing(singleTr):
     totalRecovered = tds[5]
     activeCases = tds[6]
     stateString = getText(state)
-    print(stateString)
     totalCases = toInt(totalCases)
-    print(totalCases)
     newCases = toIntNoPlus(newCases)
-    print(newCases)
     totalDeaths = toInt(totalDeaths)
-    print(totalDeaths)
     newDeaths = toIntNoPlus(newDeaths)
-    print(newDeaths)
     totalRecovered = toInt(totalRecovered)
-    print(totalRecovered)
     activeCases = toInt(activeCases)
-    print(activeCases)
+    return RowResult(stateString, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, activeCases)
 
 def toIntNoPlus(singleTD):
     strAsInput = getText(singleTD)
@@ -57,10 +64,31 @@ def getText(singleTD):
     rawText = singleTD.text
     return rawText.strip()
 
+class SingleStateScrape:
+    def __init__(self, rowResults):
+        self.rowResults = rowResults
+        self.scrapeTime = datetime.utcnow().strftime("%m/%d/%Y,%H:%M:%S")
+
+    def serialize(self):
+        result = {}
+        json_string = [ob.serialize() for ob in self.rowResults]
+        result["rowResults"] = json_string
+        result["scrapeTime"] = self.scrapeTime
+        return json.dumps(result)
+
 class RowResult:
-    def __init__(self, realpart, imagpart):
-        self.r = realpart
-        self.i = imagpart
+    def __init__(self, state, total, newCases, totalDeaths, newDeaths, totalRecovered, activeCases):
+        self.state = state
+        self.total = total
+        self.newCases = newCases
+        self.totalDeaths = totalDeaths
+        self.newDeaths = newDeaths
+        self.totalRecovered = totalRecovered
+        self.activeCases = activeCases
+
+    def serialize(self):
+        return self.__dict__
+
 
 if __name__== "__main__":
   main()
