b1468a657ef7648d953d70afcf6ec3f281fe2576
Kevin Simons <ksimons@palantir.com>
2020-03-29 13:08:17 -0700

    Start smoothing

--- main_aggregator.py
+++ main_aggregator.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import json
+from functools import cmp_to_key
 from datetime import datetime
 
 
@@ -13,12 +14,38 @@ def main():
     writeAggregatedFile("world")
 
 def writeAggregatedFile(subDir: str):
-    finalMap = createFinalMap(subDir)
+    finalMap = createFinalMap2(subDir)
     finalJSONMap = json.dumps(finalMap)
     mypath = aggregatedDataPrefix + subDir +  "/data.json"
     f = open(mypath, "w")
     f.write(finalJSONMap)
     f.close()
+
+def createFinalMap2(subDir: str):
+    mypath = scrapedDataPrefix + subDir
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    allDatas = []
+    for fileName in onlyfiles:
+        fullFileName = mypath + "/" + fileName
+        with open(fullFileName) as json_file:
+            data = json.load(json_file)
+            allDatas.append(data)
+    sortedArray = sorted(allDatas, key=lambda x: datetime.strptime(x['scrapeTime'], "%m:%d:%Y,%H:%M:%S"))
+    finalMap = {}
+    for i in range(len(sortedArray) - 1):
+        firstDay = sortedArray[i]
+        secondDay = sortedArray[i + 1]
+        firstDayDatetimeObject =  dataToDatetimeObject(firstDay)
+        secondDayDatetimeObject =  dataToDatetimeObject(secondDay)
+        firstDayDateString = firstDayDatetimeObject.date().strftime("%m/%d/%Y")
+        secondDayDateString = secondDayDatetimeObject.date().strftime("%m/%d/%Y")
+        if firstDayDateString != secondDayDateString:
+            smoothed = smoothOut(firstDay, secondDay)
+            finalMap[firstDayDateString] = smoothed
+    return finalMap
+
+def smoothOut(firstDay, secondDay):
+    return firstDay
 
 def createFinalMap(subDir: str):
     mypath = scrapedDataPrefix + subDir
@@ -39,7 +66,6 @@ def createFinalMap(subDir: str):
     for key in scrapesPerData:
         finalMap[key] = scrapesPerData[key]["rowResults"]
     return finalMap
-
 
 def dataToDatetimeObject(data):
     scrapeTime = data["scrapeTime"]
